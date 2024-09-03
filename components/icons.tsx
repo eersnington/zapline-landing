5bd6eb7dfecafc4b2f4bf926ba27c35407e87b32
Sree Narayanan <sreeaadhi07@gmail.com>
2024-09-03 17:43:50 +0400

    updates

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,28 +1,24 @@
 import * as React from "react";
+import Image from "next/image";
+import { ImageProps } from "next/image";
 
 import { IconSvgProps } from "@/types";
 
-export const Logo: React.FC<IconSvgProps> = ({
-  size = 36,
-  width,
-  height,
+export const Logo: React.FC<Omit<ImageProps, "src" | "alt">> = ({
+  width = 32,
+  height = 32,
   ...props
-}) => (
-  <svg
-    fill="none"
-    height={size || height}
-    viewBox="0 0 32 32"
-    width={size || width}
-    {...props}
-  >
-    <path
-      clipRule="evenodd"
-      d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-      fill="currentColor"
-      fillRule="evenodd"
+}) => {
+  return (
+    <Image
+      alt="Logo"
+      height={height}
+      src="/favicon-32x32.png"
+      width={width}
+      {...props}
     />
-  </svg>
-);
+  );
+};
 
 export const DiscordIcon: React.FC<IconSvgProps> = ({
   size = 24,
--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,9 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Zapline AI",
+  description:
+    "Give your brand a Voice. Elevate your CX with AI-powered Voice Support.",
   navItems: [
     {
       label: "Home",
