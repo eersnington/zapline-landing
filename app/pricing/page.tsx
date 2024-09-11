95b1b1e5e0eeb48a4595e75deab028663a360b7b
Sree Narayanan <sreeaadhi07@gmail.com>
2024-09-11 15:36:51 +0400

    update ip check

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,7 +1,27 @@
+import axios from "axios";
+
 import PricingTable from "@/components/pricing-table";
 import { title } from "@/components/primitives";
 
-export default function PricingPage() {
+const fetchUserLocation = async () => {
+  try {
+    const response = await axios.get("https://ipapi.co/json/");
+
+    console.log("User IP data:", response.data);
+
+    return response.data.country_code;
+  } catch (error) {
+    console.error("Error fetching location:", error);
+
+    return null;
+  }
+};
+
+export default async function PricingPage() {
+  const userLoc = await fetchUserLocation();
+
+  console.log(userLoc);
+
   return (
     <div>
       <h1 className={title()}>Pricing</h1>
